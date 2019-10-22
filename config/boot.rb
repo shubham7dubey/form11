2c868d5f078e027ef76da3b1e797d5be69372c23
Rahul Kalsangra <51331971+RahulKalsangra@users.noreply.github.com>
2019-10-22 23:28:38 +0530

    Update boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,13 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge(Host:  '10.11.0.60', Port: 3000)
+    end
+  end
+end
+
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
