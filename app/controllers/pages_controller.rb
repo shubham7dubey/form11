bebbe3dad5cbff54a7c772c46c0346664dc71849
Shubham Dubey <48009788+shubham7dubey@users.noreply.github.com>
2019-10-23 23:06:29 +0530

    8

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   def home
   	@name=params[:user]
   	@pass=params[:password]
+    @tempPass=params[:password]
+    @tempname=params[:user]
 
   	
 
@@ -9,5 +11,7 @@ class PagesController < ApplicationController
   def login
   	@name=params[:user]
   	@pass=params[:password]
+    @temppass=params[:password]
+    @tempname=params[:user]
   end
 end
