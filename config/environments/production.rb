94bc0b5ef66499a7b938a0a79e75e744b3c32f29
Rahul Kalsangra <51331971+RahulKalsangra@users.noreply.github.com>
2019-10-23 12:07:56 +0530

    Ip Address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,19 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default :charset =>"utf-8"
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+     address:              'smtp.gmail.com',
+     port:                 '465',
+     ssl:                   'true',
+     domain:               'gmail.com',
+     user_name:            'siddharth.jmanit@gmail.com',
+     password:             '#supreet0118',
+     authentication:       'login',
+     enable_starttls_auto: true  
+  }
+  config.action_mailer.default_url_options = { :host => '10.11.0.60' }
+
 end
